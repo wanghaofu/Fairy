b72b8a93be2913243b5989a8ca3c3b0b1a37870c
wangtao <wanghaofu@163.com>
2015-07-14 16:19:15 +0800

    x

--- test.php
+++ test.php
@@ -1,11 +1,13 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
+require_once __DIR__ . '/../../../vendor/autoload.php'; // Autoload files using Composer autoload
 
 use Fairy\Sys\Noah;
 
+echo "hello";
 // echo SayHello::world();
-var_dump(Noah::db());
+//var_dump(Noah::db());
 // Go to the terminal (or create a PHP web server inside "tests" dir) and type:
 
+Noah::db();
 // php tests/test.php
